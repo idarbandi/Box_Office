14750c7d8e05caae91cc8561338ea3fcacf25d19
idarbandi <darbandidr99@gmail.com>
2022-10-03 14:20:20 +0330

    added_Balance_check

--- transactions/admin.py
+++ transactions/admin.py
@@ -6,10 +6,11 @@ from django.contrib.admin import register
 @register(transaction)
 class TransAdmin(admin.ModelAdmin):
     list_display = ['user', 'type', 'amount', 'updateDate']
-    search_fields = ['user']
+    search_fields = ['user__username']
+    list_filter = ['type']
 
 
 @register(UserBalance)
 class BalanceUserAdmin(admin.ModelAdmin):
-    list_display = ['user', 'balance']
+    list_display = ['user']
     search_fields = ['user']
--- transactions/models.py
+++ transactions/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.functions import Coalesce
 from khayyam.jalali_datetime import JalaliDatetime
+from django.db.models import Sum, Count, Q
 
 
 class transaction(models.Model):
@@ -20,14 +22,18 @@ class transaction(models.Model):
     updateDate = models.DateTimeField(default=JalaliDatetime.today)
 
     def __str__(self):
-        return ','.join(f"\t{self.user},\t{self.__get.type._display}\t{self.amount}")
+        return f"\t{self.user},\t{self.get_type_display()}\t{self.amount}"
+
+    @classmethod
+    def calculate(cls):
+        pos_transaction = Sum('user_trans').filter(Q(transaction__type=1))
+        neg_transaction = Sum('user_trans').filter(Q(transaction__type__in=[2, 3]))
+        user = User.objects.all().aggregate(all_transactions=Count('user_trans'),
+                                            balance=Coalesce(pos_transaction) - Coalesce(neg_transaction))
+        return user
 
 
 class UserBalance(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user_balance')
     balance = models.FloatField(null=True)
     moment = models.DateTimeField(auto_now_add=JalaliDatetime.now)
-
-
-
-
